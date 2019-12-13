b560291ca5ceb14736d40f2b8a9a1de89c665f1e
Shankara <=>
2019-12-13 07:45:35 +0530

    added chai assert library and .gitignore

--- test/testLine.js
+++ test/testLine.js
@@ -1,5 +1,5 @@
 const Line = require("../src/line");
-const assert = require("assert");
+const assert = require("chai").assert;
 
 describe("toString", function() {
   it("should give the string representation of line object", function() {
@@ -14,12 +14,22 @@ describe("isEqualTo", function() {
   it("should check whether two lines are equal", function() {
     const line = new Line({ x: 3, y: 4 }, { x: 6, y: 7 });
     const actual = line.isEqualTo(new Line({ x: 3, y: 4 }, { x: 6, y: 7 }));
-    assert.strictEqual(actual, true);
+    assert.isOk(actual);
   });
 
   it("should determine if two lines are not equal", function() {
     const line = new Line({ x: 3, y: 3 }, { x: 6, y: 7 });
     const actual = line.isEqualTo(new Line({ x: 2, y: 3 }, { x: 6, y: 3 }));
-    assert.strictEqual(actual, false);
+    assert.isNotOk(actual);
+  });
+
+  it("should invalidate if other given line is not a Line object", function() {
+    const line = new Line({ x: 3, y: 3 }, { x: 6, y: 7 });
+    const otherLine = {
+      start: { x: 3, y: 3 },
+      end: { x: 6, y: 7 }
+    };
+    const actual = line.isEqualTo(otherLine);
+    assert.isNotOk(actual);
   });
 });
