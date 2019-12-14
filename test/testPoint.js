bea3579f30c6e9fb91e76c2f7a308e09c7c5a7d1
Shankara <=>
2019-12-14 13:43:05 +0530

    added visit method in Point class

--- test/testPoint.js
+++ test/testPoint.js
@@ -9,4 +9,24 @@ describe("Point", function() {
       assert.strictEqual(actual, "[Point @(3,2)]");
     });
   });
+
+  describe("#visit", function() {
+    it("should return addition of coordinates for add function", function() {
+      const p = new Point(3, 4);
+      const add = function(x, y) {
+        return x + y;
+      };
+      const actual = p.visit(add);
+      assert.strictEqual(actual, 7);
+    });
+
+    it("should return multiplication of coordinates for multiply function", function() {
+      const p = new Point(4, 5);
+      const multiply = function(x, y) {
+        return x * y;
+      };
+      const actual = p.visit(multiply);
+      assert.strictEqual(actual, 20);
+    });
+  });
 });
